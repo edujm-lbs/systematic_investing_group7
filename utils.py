0483e2bea4952cb91be27072e1abfad2b827fb82
edujm-lbs <eduj.mifpt2024@london.edu>
2023-05-21 20:13:36 +0100

    Doc

--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ import numpy as np
 
 def z_score(x):
     """
-    used to calculate z-scores
+    Normilises a set of values by calculating the Z-score.
+
+    Parameters
+    ----------
+    x : pandas.Series, numpy.Array, list of other similar iterable object
+        The value series to be normalised.
+   
+    Returns
+    -------
+    Iterable object inputted
+        The value series normilised using Z-scoring.
     """
     return (x - np.mean(x)) / np.std(x)
