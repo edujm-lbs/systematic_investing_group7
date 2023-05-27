e399c13e7f268a37ce6d9871d3152148b92a83b7
edujm-lbs <eduj.mifpt2024@london.edu>
2023-05-27 13:13:38 +0100

    Using one-hot encoding for credit rating

--- value.py
+++ value.py
@@ -1,7 +1,8 @@
 from statistics import NormalDist
 
 import numpy as np
-import statsmodels.api as sm    
+import statsmodels.api as sm
+import pandas as pd  
 
 from utils import z_score
 
@@ -26,16 +27,18 @@ def calculate_value_signals(df):
     df['spread_to_pd_res_score'] = z_score(df.spread_to_pd_res).fillna(0)
     df['spread_to_pd_res_score_sa'] = df.groupby(['sector'])['spread_to_pd_res'].apply(z_score).fillna(0)
     # TMT, TMT_2, and N_SP
-    X = df[["TMT", "TMT_2", "N_SP"]]
-    y = df["SPREAD_yield"]
+    df_w_dum = pd.get_dummies(df, columns=['N_SP'], drop_first=True)
+    n_sp_dummies_col = pd.get_dummies(df.N_SP, drop_first=True).columns.to_list()
+    X = df_w_dum[["TMT", "TMT_2"]] + n_sp_dummies_col
+    y = df_w_dum["SPREAD_yield"]
     X = sm.add_constant(X)
     reg_model = sm.OLS(y, X, missing='drop').fit()
     predicted_spread = reg_model.predict()
-    df['value_reg_richness'] = np.nan
-    df.loc[~df[["TMT", "TMT_2", "N_SP"]].isna().any(axis=1), 'value_reg_richness'] = predicted_spread
-    df['value_reg_richness'] = df['value_reg_richness'] - df['SPREAD_yield']
-    df['value_reg_richness_score_sa'] = df.groupby(['sector'])['value_reg_richness'].apply(z_score).fillna(0)
-    df['value_reg_richness_score'] = z_score(df['value_reg_richness']).fillna(0)
-    df['value_score_sa'] = df[['value_reg_richness_score_sa', 'spread_to_pd_res_score_sa']].mean(axis=1).values
-    df['value_score'] = df[['value_reg_richness_score', 'spread_to_pd_res_score']].mean(axis=1).values
+    df_w_dum['value_reg_richness'] = np.nan
+    df_w_dum.loc[~df_w_dum[["TMT", "TMT_2"]+ n_sp_dummies_col].isna().any(axis=1), 'value_reg_richness'] = predicted_spread
+    df_w_dum['value_reg_richness'] = df_w_dum['value_reg_richness'] - df_w_dum['SPREAD_yield']
+    df_w_dum['value_reg_richness_score_sa'] = df.groupby(['sector'])['value_reg_richness'].apply(z_score).fillna(0)
+    df_w_dum['value_reg_richness_score'] = z_score(df_w_dum['value_reg_richness']).fillna(0)
+    df_w_dum['value_score_sa'] = df_w_dum[['value_reg_richness_score_sa', 'spread_to_pd_res_score_sa']].mean(axis=1).values
+    df_w_dum['value_score'] = df_w_dum[['value_reg_richness_score', 'spread_to_pd_res_score']].mean(axis=1).values
     return df
