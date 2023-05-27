e033cb0aa27f4eea0ac4aed43652de1cc40ff052
edujm-lbs <eduj.mifpt2024@london.edu>
2023-05-27 10:59:32 +0100

    Changing value signal to decrease window

--- momentum.py
+++ momentum.py
@@ -30,19 +30,6 @@ def calculate_momentum_signals(df, date):
         df_isin['mom_spread_12_m_1'] = df_isin['SPREAD_yield'].diff(periods=11).shift(1)
         l_df.append(df_isin)
     df_out = pd.concat(l_df)
-    ##################################################################################
-    # Value signal that uses historic data therefore including it here with momentum #
-    ##################################################################################
-    X = df[["TMT", "TMT_2", "N_SP"]].fillna(method="ffill") 
-    y = df["SPREAD_yield"]
-    X = sm.add_constant(X)
-    reg_model = sm.OLS(y, X).fit()
-    predicted_spread = np.dot(X, reg_model.params)
-    df_out['value_reg_richness'] = predicted_spread - df_out['SPREAD_yield']
-    df_out = df_out[df_out.DATE == date]
-    df_out['value_reg_richness_score_sa'] = df_out.groupby(['sector'])['value_reg_richness'].apply(z_score).fillna(0)
-    df_out['value_reg_richness_score'] = z_score(df_out['value_reg_richness']).fillna(0)
-    # Back to momentum
     df_out['mom_spread_6_score_sa'] = df_out.groupby(['sector'])['mom_spread_6'].apply(z_score).fillna(0)
     df_out['mom_spread_6_score'] = z_score(df_out['mom_spread_6']).fillna(0)
     df_out['mom_spread_12_m_1_score_sa'] = df_out.groupby(['sector'])['mom_spread_12_m_1'].apply(z_score).fillna(0)
--- value.py
+++ value.py
@@ -16,6 +16,7 @@ def calculate_value_signals(df):
         df.apply(lambda x: None if ((x.sigma_a == 0) or (x.debt == 0) or (((x.mkvalt+x.debt)/x.debt)<0)) \
                                 else np.log((x.mkvalt+x.debt)/x.debt)/x.sigma_a, axis=1)
     df['PD'] = df.D2D.apply(lambda x: NormalDist(mu=0, sigma=1).cdf(-x))
+    # PD vs Spread yield
     Y = df.SPREAD_yield.to_numpy()
     X = df.PD.to_numpy()
     reg_model = sm.OLS(Y,X, missing='drop').fit()  # training the model
@@ -24,6 +25,18 @@ def calculate_value_signals(df):
     df.loc[~df.PD.isna(), 'spread_to_pd_res'] = residual_values
     df['spread_to_pd_res_score'] = z_score(df.spread_to_pd_res).fillna(0)
     df['spread_to_pd_res_score_sa'] = df.groupby(['sector'])['spread_to_pd_res'].apply(z_score).fillna(0)
+    # TMT, TMT_2, and N_SP
+    X = df[["TMT", "TMT_2", "N_SP"]]
+    y = df["SPREAD_yield"]
+    X = sm.add_constant(X)
+    reg_model = sm.OLS(y, X, missing='drop').fit()
+    predicted_spread = reg_model.predit()
+    df['value_reg_richness'] = np.nan
+    df.loc[~df[["TMT", "TMT_2", "N_SP"]].isna().any(axis=1).sum(), 'value_reg_richness'] = predicted_spread
+    df['value_reg_richness'] = df['value_reg_richness'] - df['SPREAD_yield']
+    df['value_reg_richness'] = predicted_spread - df['SPREAD_yield']
+    df['value_reg_richness_score_sa'] = df.groupby(['sector'])['value_reg_richness'].apply(z_score).fillna(0)
+    df['value_reg_richness_score'] = z_score(df['value_reg_richness']).fillna(0)
     df['value_score_sa'] = df[['value_reg_richness_score_sa', 'spread_to_pd_res_score_sa']].mean(axis=1).values
     df['value_score'] = df[['value_reg_richness_score', 'spread_to_pd_res_score']].mean(axis=1).values
     return df
